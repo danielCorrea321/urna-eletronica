2a19e95502df9ce12986a9b5542f76e28b7f679f
Daniel Correa <daniel.correa321@hotmail.com>
2022-07-24 20:53:15 -0300

    enviando projeto

---
+++ utils/presidentes.ts
@@ -0,0 +1,13 @@
+type Props = {
+    nome: string;
+    numero: number;
+    numero2: number;
+    partido: string;
+    img: string;
+}
+
+export const presidentes = [
+    {nome: 'Nulo', numero: 0, numero2: 0, partido: 'Nulo', img: 'https://cdn5.vectorstock.com/i/thumb-large/45/79/male-avatar-profile-picture-silhouette-light-vector-4684579.jpg'},
+    {nome: 'Jair Messias Bolsonaro', numero: 2, numero2: 2, partido: 'PL', img: 'https://static.poder360.com.br/2019/01/foto-oficial-Bolsonaro.png'},
+    {nome: 'Luis Inacio Lula da Silva', numero: 1, numero2: 3, partido: 'PT', img: 'https://pbs.twimg.com/profile_images/1518543225933512705/T4r0T3SE_400x400.jpg'},
+]
